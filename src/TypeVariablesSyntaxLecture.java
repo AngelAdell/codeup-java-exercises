9a2541f0a3db858a2d47febfae3aaed95b3cfb6c
Angel Adell <angeldeniseadell@gmail.com>
2023-12-12 15:34:51 -0500

    feat:take a quick break

---
+++ src/TypeVariablesSyntaxLecture.java
@@ -0,0 +1,29 @@
+public class TypeVariablesSyntaxLecture {
+    public static void main(String[] args) {
+        System.out.println("This is an example of a statement");
+        System.out.println("This statement must end in a semi-colon");
+
+        // In Java, every statement must end with a semicolon.
+     int someNumber = 3;
+        System.out.println(someNumber);
+        someNumber = 126;
+        someNumber++;
+        System.out.println(someNumber);
+
+//        int numDefault;
+//        numDefault = 0; // This is a statement
+//        System.out.println(numDefault);
+
+        char someLetter = 'c';
+        System.out.println(someLetter);
+
+        String someString = "Codeup is awesome!";
+        System.out.println(someString);
+
+        String quotedString = "This is a \"quoted\" String";
+        System.out.println(quotedString);
+
+        final String finalString = "This is a \"Angel's\" final string";
+
+    }
+}
